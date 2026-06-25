d4bd16b4b1e73b2a74f1eac21a85112589ff8a83
bereke1t2 <bereketaschalew1212@gmail.com>
2026-06-25 15:08:13 +0000

    refactor(usecase): update problems usecase to user-scoped interface

--- internal/usecase/problems/problems_usecase.go
+++ internal/usecase/problems/problems_usecase.go
@@ -3,6 +3,7 @@ package problems
 import "github.com/bereket/cpd-hub-backend/internal/domain"
 
 // Usecase provides business logic for problems.
+// All operations are scoped to a calling username.
 type Usecase struct {
 	repo domain.ProblemRepository
 }
@@ -11,30 +12,30 @@ func New(repo domain.ProblemRepository) *Usecase {
 	return &Usecase{repo: repo}
 }
 
-func (u *Usecase) List() ([]*domain.Problem, error) {
-	return u.repo.List()
+func (u *Usecase) ListForUser(username string) ([]*domain.Problem, error) {
+	return u.repo.ListForUser(username)
 }
 
-func (u *Usecase) GetDaily() (*domain.Problem, error) {
-	return u.repo.GetDaily()
+func (u *Usecase) GetByIDForUser(username, id string) (*domain.Problem, error) {
+	return u.repo.GetByIDForUser(username, id)
 }
 
-func (u *Usecase) Like(id string) error {
-	return u.repo.Like(id)
+func (u *Usecase) GetDailyForUser(username string) (*domain.Problem, error) {
+	return u.repo.GetDailyForUser(username)
 }
 
-func (u *Usecase) Dislike(id string) error {
-	return u.repo.Dislike(id)
+func (u *Usecase) Like(username, id string) error {
+	return u.repo.Like(username, id)
 }
 
-func (u *Usecase) MarkSolved(id string) error {
-	return u.repo.MarkSolved(id)
+func (u *Usecase) Dislike(username, id string) error {
+	return u.repo.Dislike(username, id)
 }
 
-func (u *Usecase) UnmarkSolved(id string) error {
-	return u.repo.UnmarkSolved(id)
+func (u *Usecase) MarkSolved(username, id string) error {
+	return u.repo.MarkSolved(username, id)
 }
 
-func (u *Usecase) GetById(id string)  (*domain.Problem, error){
-	return u.repo.GetById(id)
+func (u *Usecase) UnmarkSolved(username, id string) error {
+	return u.repo.UnmarkSolved(username, id)
 }
